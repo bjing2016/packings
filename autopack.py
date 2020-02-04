210b8b7f2abf0b1d3fda1497aaaa05b15a0a84b0
bjing2016 <bjing2016@github.com>
2020-02-04 16:07:00 +0000

    reruns without nans

--- autopack.py
+++ autopack.py
@@ -2,21 +2,21 @@ from packgpu import *
 import numpy as np
 
 tasks = [
-    #(32, 75),
-    #(32, 100),
-    #(32, 150),
-    #(64, 150),
-    #(64, 200),
-    #(64, 250),
-    #(64, 300),
-    #(64, 400),
-    #(64, 500),
+    (32, 75),
+    (32, 100),
+    (32, 150),
+    (64, 150),
+    (64, 200),
+    (64, 250),
+    (64, 300),
+    (64, 400),
+    (64, 500),
     #(128, 500),
     #(128, 600),
     #(128, 700),
-    #(128, 750),
-    #(128, 800),
-    #(128, 900),
+    (128, 750),
+    (128, 800),
+    (128, 900),
     #(128, 1000),
     #(256, 1000),
     #(256, 1500),
@@ -28,7 +28,7 @@ tasks = [
 ]
 
 for d, k in tasks:
-    m = orthorandom(d=d, k=k).requires_grad_()
+    m = orthorandom(d=d, k=k)
     best, betas, maxes = train(m, schedule)    
     np.save('autobest/{}.{}best.npy'.format(d, k), best)
     np.save('autobest/{}.{}betas.npy'.format(d, k), betas)
